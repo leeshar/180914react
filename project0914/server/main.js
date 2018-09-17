6be0d2eb58df6620e3e5cc5e82517b964b132873
이인호 <snrntpa2z@naver.com>
2018-09-17 21:40:29 +0900

    0917
    0917

---
+++ project0914/server/main.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import path from 'path';
+
+const app = express();
+const port = 3000;
+
+app.use('/',express.static(path.join(__dirname,'../public')));
+
+
+app.get('/hello', (req,res) => {
+    return res.send('Hello codelab');
+});
+
+app.listen(port, () =>{
+    console.log('Express is listening on port', port);
+
+});
--- project0914/src/components/AdminNav.js
+++ project0914/src/components/AdminNav.js
@@ -3,6 +3,7 @@ import {NavLink} from 'react-router-dom';
 import Arrow from '../img/menu_arrow.png';
 
 export default class AdminNav extends Component{
+   
     render(){
         return(
             <div className="adminNav">
@@ -10,7 +11,13 @@ export default class AdminNav extends Component{
             <li class="adminli">ADMIN</li>
             <li><NavLink to="/">홈</NavLink></li>
             <li>관리</li>
-            <li>회원관리<span><img src={Arrow}/></span></li>
+            <li><a>회원관리<span><img src={Arrow}/></span></a></li>
+            <ul id="submenu01">
+            <li>일반회원</li>
+            <li>스토어회원</li>
+            <li>스토어정보</li>
+            <li>휴먼회원</li>
+            </ul>
             <li>승인관리<span><img src={Arrow}/></span></li>
             <li>스토어관리<span><img src={Arrow}/></span></li>
             <li>주문/클레임관리<span><img src={Arrow}/></span></li>
